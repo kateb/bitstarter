28baaf2dbb131e8284e50aab2c3d3b11fc8bac16
kateb <bartosz.rybacki@gmail.com>
2013-07-13 23:32:06 +0200

    refactored commo lines

--- grader.js
+++ grader.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
+ Automatically grade files for the presence of specified HTML tags/attributes.
+ Uses commander.js and cheerio. Teaches command line application development
+ and basic DOM parsing.
 
-References:
+ References:
 
  + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
+ - https://github.com/MatthewMueller/cheerio
+ - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+ - http://maxogden.com/scraping-with-node.html
 
  + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ - https://github.com/visionmedia/commander.js
+ - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
  + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
+ - http://en.wikipedia.org/wiki/JSON
+ - https://developer.mozilla.org/en-US/docs/JSON
+ - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ */
 
 var fs = require('fs');
 var program = require('commander');
@@ -29,80 +29,81 @@ var HTMLFILE_DEFAULT = "index.html";
 var DOWNLOADED_HTMLFILE = "index-downloaded.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertFileExists = function(infile) {
+var assertFileExists = function (infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if (!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+var cheerioHtmlFile = function (htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
+var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
-var buildfn = function(checksfile){
+var buildfn = function (checksfile) {
 
-    var response2file = function(result, response) {
-	var resultfile = DOWNLOADED_HTMLFILE;
-	if (result instanceof Error) {
+    var response2file = function (result, response) {
+        var resultfile = DOWNLOADED_HTMLFILE;
+        if (result instanceof Error) {
             console.error('Error: ' + util.format(response.message));
-	    process.exit(1);
+            process.exit(1);
         } else {
             console.error("Wrote %s", resultfile);
             fs.writeFileSync(resultfile, result);
-            checkJson = checkHtmlFile(resultfile, program.checks);
-	    var outJson = JSON.stringify(checkJson, null, 4);
-            console.log(outJson);
+            checkHtmlFile2console(resultfile, program.checks);
         }
     };
-   return response2file;
+    return response2file;
 }
 
-var checkHtmlUrl= function(pageurl, checksfile) {
-	var response2file = buildfn(checksfile);
-	rest.get(pageurl).on('complete', response2file);
+var checkHtmlUrl = function (pageurl, checksfile) {
+    var response2file = buildfn(checksfile);
+    rest.get(pageurl).on('complete', response2file);
 }
 
-var clone = function(fn) {
+var checkHtmlFile2console = function (file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+var clone = function (fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if(require.main == module) {
+if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.option('-u, --url [html_url]', 'URL to index.html')
+        .option('-u, --url [html_url]', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = undefined;
-    if (program.url){
+    if (program.url) {
         checkHtmlUrl(program.url, program.checks);
     } else {
-	checkJson = checkHtmlFile(program.file, program.checks);
-        var outJson = JSON.stringify(checkJson, null, 4);
-        console.log(outJson);
-
+        checkHtmlFile2console(program.file, program.checks);
     }
 
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
 
